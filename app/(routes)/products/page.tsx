903a23006afab5feefd2ba0548223b60bd03523e
Mitko <mitkoiliev505@gmail.comm>
2023-12-21 21:52:50 +0100

    key prop fix

--- app/(routes)/products/page.tsx
+++ app/(routes)/products/page.tsx
@@ -13,19 +13,20 @@ export default async function Shop() {
   return (
     <div className='px-6 md:px-10 lg:px-14'>
       <div className='grid grid-cols-1 md:grid-cols-3 lg:grid gap-10'>
-        {data.map((el) => (
-          <Suspense fallback={<Loading />}>
-            <div key={el.id}>
-              <Product
-                id={el.id}
-                title={el.title}
-                category={el.category}
-                price={el.price}
-                image={el.image}
-              />
-            </div>
-          </Suspense>
-        ))}
+        {data &&
+          data.map((el, index) => (
+            <Suspense fallback={<Loading />}>
+              <div key={index}>
+                <Product
+                  id={el.id}
+                  title={el.title}
+                  category={el.category}
+                  price={el.price}
+                  image={el.image}
+                />
+              </div>
+            </Suspense>
+          ))}
       </div>
     </div>
   );
