73ceeb8b345c10e1b8eac35b615b6c040204ef5c
Mitko <mitkoiliev505@gmail.comm>
2024-02-17 22:17:29 +0100

    change: : logo name change

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,10 +3,10 @@ import { Gem } from 'lucide-react';
 export default function Logo() {
   return (
     <>
-      <div className='flex items-center z-20 cursor-pointer'>
-        <Gem size={25} />
-        <span className='uppercase text-xl ml-1'>Ultra</span>
-        <span className='font-bold uppercase text-xl'>shop</span>
+      <div className='flex items-center z-20 cursor-pointer gap-1'>
+        <span className='uppercase text-xl font-bold'>Diamond</span>
+        <Gem size={20} />
+        <span className=' uppercase text-xl'>shop</span>
       </div>
     </>
   );
