27b454070b9edd60ab23c3c74b64093a981af695
Mitko <mitkoiliev505@gmail.comm>
2024-02-23 17:58:46 +0100

    fixed: overflow-x

--- actions/fetchFew.ts
+++ actions/fetchFew.ts
@@ -1,7 +1,7 @@
 'use server';
 
 export async function fetchFew() {
-  const res = await fetch('https://fakestoreapi.com/products?limit=5');
+  const res = await fetch('https://fakestoreapi.com/products?limit=3');
   if (!res.ok) throw new Error('Failed to fetch');
   return res.json();
 }
